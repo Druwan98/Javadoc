e7d45c3c52e221150ba3faf2a5baed1588631fe9
Claudio Caporaso <claudiocaporaso1998@gmail.com>
2023-07-31 14:18:06 +0200

    Esercizio Completato

---
+++ Tester.java
@@ -0,0 +1,25 @@
+package exercises.Javadoc;
+
+/**
+ * The type Tester.
+ */
+public class Tester {
+
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        WordGames wordGames = new WordGames();
+        System.out.println(wordGames.addHelloWord("Hello"));
+        System.out.println(wordGames.getFullName("John", "Doe"));
+    }
+
+
+
+
+
+
+}
---
+++ WordGames.java
@@ -0,0 +1,30 @@
+package exercises.Javadoc;
+
+/**
+ * The type Word games.
+ */
+public class WordGames {
+    /**
+     * Add hello word string.
+     *
+     * @param word the word
+     * @return the string
+     */
+    public String addHelloWord(String word){
+        return "Hello " + word;
+    }
+
+    /**
+     * Get full name string.
+     *
+     * @param name    the name
+     * @param surname the surname
+     * @return the string
+     */
+    public String getFullName (String name, String surname){
+        return name + " " + surname;
+    }
+
+
+
+}
